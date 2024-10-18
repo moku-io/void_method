2d27cfcc041a0d769db15718de9bd41ed4fd7627
Riccardo Agatea <riccardo@moku.io>
2024-10-18 10:25:58 +0200

    add implementation

--- lib/void_method.rb
+++ lib/void_method.rb
@@ -1,1 +1,15 @@
 require_relative 'void_method/version'
+
+if Kernel.method_defined? :void
+  raise RuntimeError,
+        'Kernel#void is already defined. Previous definition is at ' \
+        "#{Kernel.instance_method(:void).source_location.join(':')}"
+end
+
+module Kernel
+  def void &block
+    nil.tap do
+      tap(&block)
+    end
+  end
+end
